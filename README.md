83dfe18ea03600c18efd6f65b61d1ade7670daea
kwells4 <kristen.wells@cuanschutz.edu>
2023-03-08 09:25:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Docker and Singularity containers
+Container image recipe repository for Docker and Singularity
+
+## About
+This repository serves as a resource for building and maintaining Docker and Singularity images for different projects.
+
+## Current recipes
+
+* Single cell RNA-seq - includes a recipe to build containers with all necessary R packages for analysis of single cell RNA-seq data. Includes instructions for using `renv` to easily add packages in the future without modifying the existing packages.
+
+
+## Planned updates
+
+* Single cell RNA-seq - include cellranger in the recipe so that the whole pipeline can be run
+* RNA-seq
+* Bulk ATAC-seq
---
+++ single_cell/R_dependencies.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(BiocManager)
+library(Seurat)
+library(biomaRt) # bioc
+library(scAnalysisR) # CUAnschutzBDC/scAnalysisR
+library(clustifyr) # bioc
+library(DoubletFinder) # chris-mcginnis-ucsf/DoubletFinder
+library(cowplot)
+library(here)
+library(pheatmap)
+library(dsb)
+library(clustree)
+library(harmony)
+library(viridis)
+library(pathview) # bioc
+library(openxlsx)
+library(gprofiler2)
+library(singlecellmethods) # immunogenomics/singlecellmethods
+library(batchelor) # bioc
+library(knitr)
+library(LaCroixColoR) # johannesbjork/LaCroixColoR
+library(rmarkdown)
+library(SingleCellExperiment)
+library(MetBrewer) # BlakeRMills/MetBrewer
+library(SignacX)
