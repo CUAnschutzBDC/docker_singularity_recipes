6fba7bb00c51478aad83fdb9ee357d10707938b4
kwells4 <kristen.wells-wrasman@cuanschutz.edu>
2025-05-07 17:04:07 -0600

    making singularity image

---
+++ RNA_seq/general_docker/create_sif.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#SBATCH --job-name=sif
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=6gb
+#SBATCH --output=logs/sif.out
+#SBATCH --partition=amilan
+
+apptainer build rnaseq_general_v2.sif Singularity
