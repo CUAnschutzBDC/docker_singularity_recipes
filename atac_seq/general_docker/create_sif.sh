cb2293a741bb19a354b26d33fce49f841d632f52
kwells4 <kristen.wells@cuanschutz.edu>
2025-05-09 08:50:36 -0600

    updating atac_seq general docker

--- atac_seq/general_docker/create_sif.sh
+++ atac_seq/general_docker/create_sif.sh
@@ -2,16 +2,9 @@
 
 #SBATCH --job-name=sif
 #SBATCH --ntasks=1
-#SBATCH --time=04:00:00
-#SBATCH --mem=3gb
+#SBATCH --time=12:00:00
+#SBATCH --mem=6gb
 #SBATCH --output=logs/sif.out
 #SBATCH --partition=amilan
 
-singularity_image=chip_v2.sif
-docker_image=chip_v2.tar
-
-
-singularity build \
-   --force \
-   $singularity_image \
-   docker-archive:$docker_image
+apptainer build rnaseq_general_v2.sif Singularity
