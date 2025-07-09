38f15d03e114789e58f1433e31dbe66506f69f37
kwells4 <kristen.wells@cuanschutz.edu>
2025-07-09 09:55:44 -0600

    updating mixcr license

---
+++ mixcr/mixcr/create_sif_dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+#SBATCH --job-name=sif
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=6gb
+#SBATCH --output=logs/sif.out
+#SBATCH --partition=amilan
+
+singularity pull --name mixcr_4.6.0.sif docker://kwellswrasman/mixcr:v2
